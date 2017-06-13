f5384df7e79501a642b0a499b91663f7ce6be058
Vlad <vladislav.mokriy@isells.eu>
2017-06-13 09:08:51 +0300

    fixes

--- client/index/index.js
+++ client/index/index.js
@@ -8,3 +8,18 @@ Template.index.helpers({
         return !!(user && user.roles && user.roles.length);
     }
 });
+
+Template.start.events({
+    'submit #loginForm': function (e) {
+        e.preventDefault();
+        let form = $(e.target);
+        let email = form.find('#email').val().trim();
+        let password = form.find('#password').val().trim();
+        Meteor.loginWithPassword(email, password, function (err) {
+            debugger;
+            if (!err) {
+                Router.go('dashboard');
+            }
+        });
+    }
+});
